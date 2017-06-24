4eff6f38c7af808132899ebbbac4a7a649df09b5
Sungsu Lim <ss.sungsulim@gmail.com>
2017-06-23 21:35:10 -0700

    commit

---
+++ server.js
@@ -0,0 +1,18 @@
+
+const express = require ('express')
+const bodyparser = require('body-parser')
+const cors = require('cors')
+const app = express()
+
+app.use(bodyparser())
+app.use(cors())
+app.get('/', function(req,res){
+    console.log("got request")
+    console.log(req.body)
+    res.send("hello world!")
+})
+
+
+app.listen(3000, function(){
+    console.log("app listening on 3000")
+})
